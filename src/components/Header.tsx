06f5a0d955b587e7e5c76259564b652dd3cf81c2
simy46 <missipsa.annane@polymtl.ca>
2025-05-22 17:19:32 -0400

    rounded logo on mobile

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -68,16 +68,20 @@ const Header = () => {
         <div className="flex items-center space-x-3">
           <a href="/" className="group flex items-center space-x-3">
             <div
-              className="h-12 w-12 rounded-full overflow-hidden  
-                        group-hover:shadow-xl group-hover:scale-105 
-                        transition-all duration-300 flex items-center justify-center"
+              className="overflow-hidden 
+                        transition-all duration-300 
+                        flex items-center justify-center 
+                        rounded-full md:rounded-none 
+                        h-12 w-12 md:h-auto md:w-auto
+                        group-hover:shadow-xl group-hover:scale-105"
             >
               <img
                 src="/logo.png"
                 alt="Logo Mr. Clear"
-                className="h-10 w-10 md:h-12 md:w-12 object-contain"
+                className="object-contain h-10 w-10 md:h-12 md:w-12"
               />
             </div>
+
             <span className="hidden md:inline-block font-semibold text-xl text-brand-blue tracking-wide group-hover:text-brand-blue/80 transition-colors duration-300">
               Mr. Clear
             </span>
